c16e0e9469974ce7a93dfdcfc918545d0b5aaaed
Zaid7821 <113490979+Zaid7821@users.noreply.github.com>
2025-12-06 22:16:52 +0530

    added render

--- server/db.js
+++ server/db.js
@@ -25,28 +25,53 @@
 
 
 // server/db.js
+// import pg from "pg";
+
+// const { Pool } = pg;
+
+// const dbConfig = {
+//     connectionString:
+//         "postgresql://neondb_owner:npg_xIVX8mJlEFQ1@ep-shiny-flower-ahbfi74u-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+//     ssl: {
+//         rejectUnauthorized: false, // required for Neon SSL
+//     },
+// };
+
+// console.log("🌐 Using Neon DB for contact messages");
+
+// const pool = new Pool(dbConfig);
+
+// pool.on("connect", () => {
+//     console.log("🔗 Successfully connected to Neon Postgres! 🌎");
+// });
+
+// pool.on("error", (err) => {
+//     console.error("❌ Unexpected error on idle client", err);
+//     process.exit(1);
+// });
+
+// export default pool;
+
+
+// -----render---------
+
+// server/db.js
 import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config(); // Read DATABASE_URL from env
 
 const { Pool } = pg;
 
-const dbConfig = {
-    connectionString:
-        "postgresql://neondb_owner:npg_xIVX8mJlEFQ1@ep-shiny-flower-ahbfi74u-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
     ssl: {
-        rejectUnauthorized: false, // required for Neon SSL
+        rejectUnauthorized: false, // Required for Neon
     },
-};
-
-console.log("🌐 Using Neon DB for contact messages");
-
-const pool = new Pool(dbConfig);
-
-pool.on("connect", () => {
-    console.log("🔗 Successfully connected to Neon Postgres! 🌎");
 });
 
 pool.on("error", (err) => {
-    console.error("❌ Unexpected error on idle client", err);
+    console.error("Unexpected error on idle client", err);
     process.exit(1);
 });
 
